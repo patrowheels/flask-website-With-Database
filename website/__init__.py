e8130a7b7231d81f148a05af8263b57b6c6dc7bd
Your Name <you@example.com>
2022-10-16 14:28:54 -0700

    update

--- website/__init__.py
+++ website/__init__.py
@@ -1,5 +1,11 @@
 #  we have to import flask
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+#
+db = SQLAlchemy()
+DB_NAME = "database.db"
+
 
 # whenerver you put __init__.py  inside of a folder that folder becomes a pyhthon package file it becomes a package
 
@@ -9,6 +15,8 @@ from flask import Flask
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = "jwhebffbkefb"
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    db.init_app(app)
 
     from .views import views
     from .auth import auth
--- website/auth.py
+++ website/auth.py
@@ -1,28 +1,53 @@
 # here we are importing the needed blueprint method and render template method from flask
-from flask import Blueprint, render_template
+from unicodedata import category
+from flask import Blueprint, render_template, request, flash
 
 # this is how you define a blueprint and you chooses to call it views.
 auth = Blueprint('auth', __name__)
 
+# methods allow this route to have get and post requests
 
-@auth.route('/login')
+
+@auth.route('/login', methods=["GET", "POST"])
 def login():
     # return "<p>Login</p>"
+    data = request.form
+    print(data)
     return render_template("login.html")
 
 
-@auth.route('/logout')
+@auth.route('/logout',)
 def logout():
     return "<p>Logout</p>"
 
 
-@auth.route('/sign-up')
+@auth.route('/sign-up', methods=["GET", "POST"])
 def sign_up():
     # return "<p>Sign Up</p>"
+    data = request.form
+    print(data)
+
+    if request.method == "POST":
+        email = request.form.get("email")
+        firstName = request.form.get("firstName")
+        password1 = request.form.get("password1")
+        password2 = request.form.get("password2")
+
+        if len(email) < 4:
+            flash("Email must be greater than 4 characters.", category="error")
+        elif len(firstName) < 2:
+            flash("First name must be greater than 1 character.", category="error")
+        elif password1 != password2:
+            flash("Passwords don\'t match", category="error")
+        elif len(password1) < 7:
+            flash("Password must be at least 7 characters", category="error")
+        else:
+            flash("Account created!", category="Success")
+
     return render_template("sign_up.html")
 
 
-@auth.route('/bonus')
+@auth.route('/bonus', methods=["GET", "POST"])
 def bonus():
     # return "<p>Sign Up</p>"
     return render_template("bonus.html")
