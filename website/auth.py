6717761acbbbe1447376b77bdb6553ad9acccf5c
Your Name <patrowheels7@gmail.com>
2022-10-22 21:47:57 -0700

    PR #3

--- website/auth.py
+++ website/auth.py
@@ -6,6 +6,7 @@ from .models import User
 # lets us store a password as a more secured data
 from werkzeug.security import generate_password_hash, check_password_hash
 from . import db
+from flask_login import login_user, login_required, logout_user, current_user
 
 # this is how you define a blueprint and you chooses to call it views.
 auth = Blueprint('auth', __name__)
@@ -15,15 +16,30 @@ auth = Blueprint('auth', __name__)
 
 @auth.route('/login', methods=["GET", "POST"])
 def login():
-    # return "<p>Login</p>"
-    data = request.form
-    print(data)
-    return render_template("login.html")
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+
+        user = User.query.filter_by(email=email).first()
+        if user:
+            if check_password_hash(user.password, password):
+                flash('Logged in successfully!', category='success')
+                login_user(user, remember=True)
+                return redirect(url_for('views.home'))
+            else:
+                flash('Incorrect password, try again.', category="error")
+        else:
+            flash('Email does not exist.', category='error')
+
+    return render_template("login.html", user=current_user)
 
 
 @auth.route('/logout',)
+# this decorator doesnt allow you to access the route unless your logged in
+@login_required
 def logout():
-    return "<p>Logout</p>"
+    logout_user()
+    return redirect(url_for('auth.login'))
 
 
 @auth.route('/sign-up', methods=["GET", "POST"])
@@ -38,7 +54,11 @@ def sign_up():
         password1 = request.form.get("password1")
         password2 = request.form.get("password2")
 
-        if len(email) < 4:
+        user = User.query.filter_by(email=email).first()
+
+        if user:
+            flash('email already exists.', category="error")
+        elif len(email) < 4:
             flash("Email must be greater than 4 characters.", category="error")
         elif len(first_name) < 2:
             flash("First name must be greater than 1 character.", category="error")
@@ -52,12 +72,14 @@ def sign_up():
             db.session.add(new_user)
             db.session.commit()
             flash("Account created!", category="Success")
+            login_user(user, remember=True)
+
             return redirect(url_for('views.home'))
 
-    return render_template("sign_up.html")
+    return render_template("sign_up.html", user=current_user)
 
 
 @auth.route('/bonus', methods=["GET", "POST"])
 def bonus():
     # return "<p>Sign Up</p>"
-    return render_template("bonus.html")
+    return render_template("bonus.html", user=current_user)
--- website/views.py
+++ website/views.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, render_template
+from flask_login import login_required, current_user
 
 # this is how you define a blueprint and you choose to call it views.
 views = Blueprint('views', __name__)
@@ -7,6 +8,7 @@ views = Blueprint('views', __name__)
 
 
 @views.route('/')
+@login_required
 def home():
     # return "<h1>Home</h1>""<h1>Welcome</h1>"
-    return render_template("home.html")
+    return render_template("home.html", user=current_user)
